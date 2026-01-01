f1b131f48895e30841af7a882aba84bb62a3675f
Nevo Kaplan <nevokaplan2@gmail.com>
2026-01-01 18:03:36 +0200

    Changed names

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ def main() -> None:
 
     current_month = 1
 
-    p1 = Person("Nevo", [date(2026, 1, 1), date(2026, 1, 20), date(2026, 1, 30)], all_rules)
+    p1 = Person("Ne", [date(2026, 1, 1), date(2026, 1, 20), date(2026, 1, 30)], all_rules)
     p1.add_time_period_unavailablity(date(2026, 1, 8), date(2026, 1, 17))
-    p2 = Person("Shira", [date(2026, 1, 2), date(2026, 1, 20), date(2026, 1, 21)], all_rules)
-    p3 = Person("Eidlin", [date(2026, 1, 1)], all_rules)
+    p2 = Person("Sh", [date(2026, 1, 2), date(2026, 1, 20), date(2026, 1, 21)], all_rules)
+    p3 = Person("Ei", [date(2026, 1, 1)], all_rules)
     p3.add_recurring_month_unavailabilities(current_month=current_month, recurring_isoday=7)
     p3.add_recurring_month_unavailabilities(current_month=current_month, recurring_isoday=3)
-    p5 = Person("Itay", [date(2026, 1, 1), date(2026, 1, 13), date(2026, 1, 29)], all_rules)
+    p5 = Person("It", [date(2026, 1, 1), date(2026, 1, 13), date(2026, 1, 29)], all_rules)
     p5.add_recurring_month_unavailabilities(current_month=current_month, recurring_isoday=3)
-    p6 = Person("Nivi", [date(2026, 1, 2)], all_rules)
+    p6 = Person("Ni", [date(2026, 1, 2)], all_rules)
 
     my_calender = Calender([p1, p2, p3, p5, p6])
     my_calender.on_calling(current_month)
