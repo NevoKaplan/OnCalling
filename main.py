6f832b123531401a492a309f615c7324875dd9ce
Nevo Kaplan <89150900+NevoKaplan@users.noreply.github.com>
2026-02-16 20:37:38 +0200

    Added comments in main

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ from rule import is_date_after_date, is_date_weekend_after_weekend, is_date_two_
 
 def main() -> None:
     all_calendar_rules = [(is_date_after_date, 90), (is_date_weekend_after_weekend, 80), (is_date_two_days_apart, 75)]
-    personal_eid_rules = [(partial(is_date_less_desired_weekday, 4), 65)]
-
     current_month = date.today().month  # Can also be a number for future months...
 
+    
+    # You can change the following code in order to suit your specific case:
+    
+    # Create as many personal rules as needed:
+    personal_eid_rules = [(partial(is_date_less_desired_weekday, 4), 65)]
+    
+    # Create people objects and add unavailablities
     p1 = Person("Ne", [date(2026, current_month, 19)])
     p1.add_time_period_unavailablity(date(2026, current_month, 2), date(2026, current_month, 10))
     p2 = Person("Sh", [])
@@ -21,8 +26,11 @@ def main() -> None:
     p3.add_recurring_month_unavailabilities(current_month=current_month, recurring_isoday=7)
     p4 = Person("Ni", [date(2026, current_month, 19)])
     p4.add_time_period_unavailablity(date(2026, current_month, 5), date(2026, current_month, 9))
-
+    # Create a list of all the people you've created
     people = [p1, p2, p3, p4]
+
+    # End of user input. Don't touch the following code.
+    
     calendars = []
     for i in range(5):
         my_calender = OnCallCalendar(copy.deepcopy(people), all_calendar_rules)
